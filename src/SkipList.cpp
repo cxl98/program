072246efa248d73ab2812c9930f9d8ef409fcbf9
cxl <1799359457@qq.com>
2021-01-06 21:03:42 +0800

    Initial commit

---
+++ src/SkipList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cxl on 2021/1/6.
+//
+
+#include "SkipList.h"
---
+++ src/SkipList.h
@@ -0,0 +1,22 @@
+//
+// Created by cxl on 2021/1/6.
+//
+
+#ifndef PROGRAM_SKIPLIST_H
+#define PROGRAM_SKIPLIST_H
+const int maxLevel=16;
+template<class T>
+class SkipListNode{
+    public
+};
+template<class T>
+class SkipList {
+public:
+    SkipList(){}
+    T key;
+    SkipList **next;
+};
+
+
+
+#endif //PROGRAM_SKIPLIST_H
