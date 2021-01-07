f00d47c63235ab585b230f457ae8fa2f3045bd6f
cxl <1799359457@qq.com>
2021-01-07 19:35:06 +0800

    update

--- src/SkipList.h
+++ src/SkipList.h
@@ -4,19 +4,37 @@
 
 #ifndef PROGRAM_SKIPLIST_H
 #define PROGRAM_SKIPLIST_H
-const int maxLevel=16;
+const int maxLevel = 16;
+
 template<class T>
-class SkipListNode{
-    public
+class SkipListNode {
+public:
+    SkipListNode() {}
+    T key;
+    SkipListNode **next;
 };
+
 template<class T>
 class SkipList {
 public:
-    SkipList(){}
-    T key;
-    SkipList **next;
-};
+    SkipList();
+    bool isEmpty() const;
+    void choosePowers();
+    int chooseLevel();
+    T* skipListSearch(const T&);
+    void skipListInsert(const T&);
 
+private:
+    typedef SkipListNode<T> *nodePtr;
+    nodePtr root[maxLevel];
+    int powers[maxLevel];
+};
+template<class T>
+SkipList<T>::SkipList() {
+    for (int i = 0; i < maxLevel; ++i) {
+        root[i]=0;
+    }
+}
 
 
 #endif //PROGRAM_SKIPLIST_H
